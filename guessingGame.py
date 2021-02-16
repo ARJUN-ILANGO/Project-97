812d71850801be16ecbd74e949571fc00ee2a880
ARJUN2009 <68690799+ARJUN-ILANGO@users.noreply.github.com>
2021-02-16 19:19:18 +0530

    Add files via upload

---
+++ guessingGame.py
@@ -0,0 +1,45 @@
+Number ='Number Guessing Game'
+print(Number)
+Digit ='Guess A Number (between 1 and 10)'
+print(Digit)
+
+
+digit=int(input('1. Enter Your Guess:-'))
+if(digit<4):
+    print('Your Guess Is Too Low ')
+elif(digit<9):
+    print('Congratulation YOU WON!!!')
+else:
+    print('Your guess was too low : Guess a number higher than this number.')
+
+number1=int(input('2.Enter Your Guess:-'))
+if(number1<4):
+  print('Your guess was too low : Guess a number higher than this number.')
+elif(number1<9):
+  print("CONGATULATION YOU WON!!!!!!!!")
+else:
+  print("Your guess was too low : Guess a number higher than this number.")
+
+number2=int(input('3.Enter Your Guess:-'))
+if(number2<4):
+  print('Your guess was too low : Guess a number higher than this number.')
+elif(number2<9):
+  print("CONGATULATION YOU WON!!!!!!!!")
+else:
+  print("Your guess was too low : Guess a number higher than this number.")
+
+number3=int(input('4.Enter Your Guess:-'))
+if(number3<4):
+  print('Your guess was too low : Guess a number higher than this number.')
+elif(number3<9):
+  print("CONGATULATION YOU WON!!!!!!!!")
+else:
+  print("Your guess was too low : Guess a number higher than this number.")
+
+numbe4=int(input('5.Enter Your Guess:-'))
+if(number4<4):
+  print('Your guess was too low : Guess a number higher than this number.')
+elif(number4<9):
+  print("CONGATULATION YOU WON!!!!!!!!")
+else:
+  print("Your guess was too low : Guess a number higher than this number.")
